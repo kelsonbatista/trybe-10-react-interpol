333f2ac61a29c3c2dd1a909d3dac74f3e28790e6
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-12-11 18:33:04 -0300

    Initial commit

---
+++ interpol/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Interpol Wanted</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
